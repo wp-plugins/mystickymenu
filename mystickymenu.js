bac2ed1141531eb536a9225c5b10db13eadc8c5a
damiroquai <damiroquai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-18 09:43:06 +0000

    ie scrollTop fix
    git-svn-id: https://plugins.svn.wordpress.org/mystickymenu/trunk@877055 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mystickymenu.js
+++ mystickymenu.js
@@ -1,7 +1,9 @@
 var mysticky_navbar = document.querySelector(mysticky_name.mysticky_string);
 var origOffsetY = mysticky_navbar.offsetTop;
-function onScroll(e) {
-  window.scrollY >= origOffsetY ? mysticky_navbar.classList.add('myfixed') :
-                                  mysticky_navbar.classList.remove('myfixed');
+var hasScrollY = 'scrollY' in window;
+
+function onScroll(e) {	
+  var y = hasScrollY ? window.scrollY : document.documentElement.scrollTop;
+  y >= origOffsetY ? mysticky_navbar.classList.add('myfixed') : mysticky_navbar.classList.remove('myfixed');
 }
 document.addEventListener('scroll', onScroll);
