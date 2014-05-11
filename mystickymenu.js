e1ab27f3313314239bce0046b0ee608d18c85ca7
damiroquai <damiroquai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-11 20:42:04 +0000

    update version 1.5
    git-svn-id: https://plugins.svn.wordpress.org/mystickymenu/trunk@912269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mystickymenu.js
+++ mystickymenu.js
@@ -2,26 +2,39 @@
 var mysticky_navbar = document.querySelector(mysticky_name.mysticky_string);
 
 // add mysticky_wrap div around selected mysticky class
-var myparent = mysticky_navbar.parentNode;
-var mywrapper = document.createElement('div');
+var parentmysticky = mysticky_navbar.parentNode;
+var wrappermysticky = document.createElement('div');
 var position = 0;
-for(var i = 0; i < myparent.childNodes.length; i++) {
-  if(myparent.childNodes[i] == mysticky_navbar) {
+for(var i = 0; i < parentmysticky.childNodes.length; i++) {
+  if(parentmysticky.childNodes[i] == mysticky_navbar) {
     position = i;
     break;
   };
 };
-mywrapper.id = 'mysticky_wrap';
-mywrapper.appendChild(mysticky_navbar);
-myparent.insertBefore(mywrapper, myparent.childNodes[position]);
-var mydivHeight = ((mysticky_navbar.offsetHeight) + 'px');
-mywrapper.style.height = mydivHeight;
+wrappermysticky.id = 'mysticky-wrap';
+wrappermysticky.appendChild(mysticky_navbar);
+parentmysticky.insertBefore(wrappermysticky, parentmysticky.childNodes[position]);
 
-// add myfixed class to selected mysticky class
-var origOffsetY = mysticky_navbar.offsetTop + 100;
+// add mysticky_nav div inside selected mysticky class
+var parentnav = mysticky_navbar.parentNode;
+var wrappernav = document.createElement('div');
+wrappernav.id = 'mysticky-nav';
+parentnav.replaceChild(wrappernav, mysticky_navbar);
+wrappernav.appendChild(mysticky_navbar);
+
+// add myfixed and wrapfixed class to divs while scroll
+var origOffsetY = mysticky_navbar.offsetTop + 100 ;
 var hasScrollY = 'scrollY' in window;
 function onScroll(e) {
-  var y = hasScrollY ? window.scrollY : document.documentElement.scrollTop;
-  y >= origOffsetY  ? mysticky_navbar.classList.add('myfixed') : mysticky_navbar.classList.remove('myfixed');
+var mydivWidth = ((mysticky_navbar.offsetWidth) + 'px');
+var mydivHeight = ((mysticky_navbar.offsetHeight) + 'px');
+var mydivReset = '';
+    // mysticky_navbar.style.width = mydivWidth;
+var y = hasScrollY ? window.scrollY : document.documentElement.scrollTop;
+	y >= origOffsetY  ? mysticky_navbar.classList.add('myfixed') : mysticky_navbar.classList.remove('myfixed');
+	y >= origOffsetY  ? wrappernav.classList.add('wrapfixed') : wrappernav.classList.remove('wrapfixed');
+	y >= origOffsetY  ? mysticky_navbar.style.width = mydivWidth : mysticky_navbar.style.width = mydivReset;
+	y >= origOffsetY  ? wrappermysticky.style.height = mydivHeight : wrappermysticky.style.height = mydivReset;
 }
+
 document.addEventListener('scroll', onScroll);
